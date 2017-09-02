f9915ebba835e2cea31b278ab2e7c1e0b4fb8059
MrFuentastic <vfuentes8587@gmail.com>
2017-09-01 22:49:08 -0500

    continued working on tic tac toe stuff...

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,3 +1,9 @@
-document.getElementById("box").onclick() = function(){
+$( document ).ready(function() {
+  var oPic = "https://orig05.deviantart.net/570e/f/2012/051/8/0/circulos_png_by_dulcezavala-d4qg7zf.png";
+  // var xPic = ;
   
-}
+  var x = [];
+  var o = [];
+  var player = x;
+  document.getElementsByClassName(cell)[0].getElementById("1").src = "http://icons.iconarchive.com/icons/gordon-irving/x-set/128/X-icon.png";
+});
