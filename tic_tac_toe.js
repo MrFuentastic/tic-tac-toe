44da715a5e38586f3f8fcb3cef7ff7c515bdb148
MrFuentastic <vfuentes8587@gmail.com>
2017-09-03 00:48:03 -0500

    switched clickiness to jquery... evertything might be jquery

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,31 +1,16 @@
-  
-  var oPic = "https://orig05.deviantart.net/570e/f/2012/051/8/0/circulos_png_by_dulcezavala-d4qg7zf.png";
-var xPic = "http://icons.iconarchive.com/icons/gordon-irving/x-set/128/X-icon.png";
-var x = [];
-var o = [];
-var player = x;
 $(document).ready(function(){
-
-  $(".xo").click(function(){
-    $(this).attr('src', xPic).removeClass("xo");
+  var oPic = "https://orig05.deviantart.net/570e/f/2012/051/8/0/circulos_png_by_dulcezavala-d4qg7zf.png";
+  var xPic = "http://icons.iconarchive.com/icons/gordon-irving/x-set/128/X-icon.png";
+  var x = [];
+  var o = [];
+  var player = x;
+  $(".xo").one('click', function(){
+    if (player == x){
+      $(this).attr('src', xPic);
+      player = o;
+    }else{
+      $(this).attr('src', oPic)
+      player = x;
+    }
   });
 });
-
-  // function addPic(cell){  
-  //   if (cell.getElementsByClassName('pic')[0]){
-  //     if (player == x) {
-  //       x.push(cell.id)
-  //       cell.getElementsByClassName('pic')[0].src = xPic;
-  //       cell.getElementsByClassName('pic')[0].classList.replace("pic", "xo");
-  //       player = o;
-  //     }else{
-  //       o.push(cell.id)
-  //       cell.getElementsByClassName('pic')[0].src = oPic;
-  //       cell.getElementsByClassName('pic')[0].classList.replace("pic", "xo");
-  //       player = x;
-  //     }
-  //   }else{
-  //     return;
-  //   }
-  // };
-
