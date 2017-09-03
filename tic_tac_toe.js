f8b43b3188544c54b5eaa4d594ad142462396048
MrFuentastic <vfuentes8587@gmail.com>
2017-09-02 23:36:48 -0500

    continued tic tac toe game

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,11 +1,31 @@
-
-  var oPic = "https://orig05.deviantart.net/570e/f/2012/051/8/0/circulos_png_by_dulcezavala-d4qg7zf.png";
-  var xPic = "http://icons.iconarchive.com/icons/gordon-irving/x-set/128/X-icon.png";
   
-  var x = [];
-  var o = [];
-  var player = x;
+  var oPic = "https://orig05.deviantart.net/570e/f/2012/051/8/0/circulos_png_by_dulcezavala-d4qg7zf.png";
+var xPic = "http://icons.iconarchive.com/icons/gordon-irving/x-set/128/X-icon.png";
+var x = [];
+var o = [];
+var player = x;
+$(document).ready(function(){
 
-  function addPic(cell){
-      cell.getElementsByClassName('pic')[0].setAttribute('src', xPic);
-    };
+  $(".xo").click(function(){
+    $(this).attr('src', xPic).removeClass("xo");
+  });
+});
+
+  // function addPic(cell){  
+  //   if (cell.getElementsByClassName('pic')[0]){
+  //     if (player == x) {
+  //       x.push(cell.id)
+  //       cell.getElementsByClassName('pic')[0].src = xPic;
+  //       cell.getElementsByClassName('pic')[0].classList.replace("pic", "xo");
+  //       player = o;
+  //     }else{
+  //       o.push(cell.id)
+  //       cell.getElementsByClassName('pic')[0].src = oPic;
+  //       cell.getElementsByClassName('pic')[0].classList.replace("pic", "xo");
+  //       player = x;
+  //     }
+  //   }else{
+  //     return;
+  //   }
+  // };
+
