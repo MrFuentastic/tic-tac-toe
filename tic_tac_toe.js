89d89a7ddb7915264ac2901b7e23160e235d8394
MrFuentastic <vfuentes8587@gmail.com>
2017-09-03 19:05:08 -0500

    started on win condition code

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -4,12 +4,27 @@ $(document).ready(function(){
   var x = [];
   var o = [];
   var player = x;
+  var winMoves = [[1,2,3],[4,5,6],[7,8,9],[1,4,6],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+  
+  var checkWin = function(player){
+    winMoves.forEach(function(winMove){
+      win = false;
+      winMove.forEach(pos){
+        if player.includes(pos)
+      }
+    });
+  }
+
   $(".xo").one('click', function(){
     if (player == x){
+      x.push($(this).attr('id'));
       $(this).attr('src', xPic);
+      checkWin(player);
       player = o;
     }else{
+      o.push($(this).attr('id'));      
       $(this).attr('src', oPic)
+      checkWin(player);
       player = x;
     }
   });
